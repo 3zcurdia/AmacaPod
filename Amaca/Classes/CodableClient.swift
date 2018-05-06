dbe52f5680bd90221f511295e486e42bfb063c31
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2018-05-06 10:05:09 -0500

    move cache manager to response handler

--- Amaca/Classes/CodableClient.swift
+++ Amaca/Classes/CodableClient.swift
@@ -26,50 +26,28 @@ public class CodableClient<T>: Clientable where T: Codable {
 
     public func listTaskFor(request: URLRequest, cache: Bool = false,
                      completionHandler: @escaping (DecodableResponseHandler<[T]>) -> Void) {
-        let manager = CacheManager<[T]>()
-        var decodedResponse = DecodableResponseHandler<[T]>()
-        decodedResponse.decoder = self.decoder
-        if let url = request.url {
-            if cache, let content = manager.find(url: url) {
-                decodedResponse.data = content
-                decodedResponse.status = .success
-                completionHandler(decodedResponse)
-                return
-            } else {
-                manager.deleteFileFrom(url: url)
-            }
+        var decodedResponse = DecodableResponseHandler<[T]>(decoder: decoder, request: request, cache: cache)
+        if decodedResponse.status == .success {
+            completionHandler(decodedResponse)
+            return
         }
         let task = config.session.dataTask(with: request) { (data, response, error) in
             decodedResponse.parse(data: data, response: response, error: error)
             DispatchQueue.main.async { completionHandler(decodedResponse) }
-            if cache, let url = request.url, let decodedData = decodedResponse.data {
-                _ = manager.save(url: url, rawData: data, jsonData: decodedData)
-            }
         }
         task.resume()
     }
 
     public func taskFor(request: URLRequest, cache: Bool = false,
                      completionHandler: @escaping (DecodableResponseHandler<T>) -> Void) {
-        let manager = CacheManager<T>()
-        var decodedResponse = DecodableResponseHandler<T>()
-        decodedResponse.decoder = self.decoder
-        if let url = request.url {
-            if cache, let content = manager.find(url: url) {
-                decodedResponse.data = content
-                decodedResponse.status = .success
-                completionHandler(decodedResponse)
-                return
-            } else {
-                manager.deleteFileFrom(url: url)
-            }
+        var decodedResponse = DecodableResponseHandler<T>(decoder: decoder, request: request, cache: cache)
+        if decodedResponse.status == .success {
+            completionHandler(decodedResponse)
+            return
         }
         let task = config.session.dataTask(with: request) { (data, response, error) in
             decodedResponse.parse(data: data, response: response, error: error)
             DispatchQueue.main.async { completionHandler(decodedResponse) }
-            if cache, let url = request.url, let decodedData = decodedResponse.data {
-                _ = manager.save(url: url, rawData: data, jsonData: decodedData)
-            }
         }
         task.resume()
     }
--- Amaca/Classes/DecodableResponseHandler.swift
+++ Amaca/Classes/DecodableResponseHandler.swift
@@ -7,19 +7,33 @@
 
 import Foundation
 
-public struct DecodableResponseHandler<T>:ResponseHandler where T: Decodable {
+public struct DecodableResponseHandler<T>:ResponseHandler where T: Codable {
     public typealias DataType = T
     public var status: StatusCode = .unkown
     public var response: HTTPURLResponse?
     public var error: Error?
     public var data: DataType?
 
-    public var decoder = JSONDecoder()
+    let cache: Bool
+    let manager = CacheManager<T>()
+    let decoder: JSONDecoder
 
-    public init() {}
+    public init(decoder: JSONDecoder, request: URLRequest, cache: Bool) {
+        self.decoder = decoder
+        self.cache = cache
+        guard let url = request.url else { return }
+        if self.cache, let content = manager.find(url: url) {
+            self.data = content
+            self.status = .success
+        } else {
+            self.manager.deleteFileFrom(url: url)
+        }
+    }
 
     public init(data: Data?, response: URLResponse?, error: Error?) {
-        parse(data: data, response: response, error: error)
+        self.decoder = JSONDecoder()
+        self.cache = false
+        self.parse(data: data, response: response, error: error)
     }
 
     mutating func parse(data: Data?, response: URLResponse?, error: Error?) {
@@ -47,5 +61,13 @@ public struct DecodableResponseHandler<T>:ResponseHandler where T: Decodable {
         }
         self.data = json
         self.status = status
+        self.saveCache(rawData: data)
+    }
+
+    func saveCache(rawData: Data?) {
+        guard self.cache else { return }
+        if let url = response?.url, let jsonData = data {
+            _ = manager.save(url: url, rawData: rawData, jsonData: jsonData)
+        }
     }
 }
